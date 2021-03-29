2de592ec47ec57075a1647bcdccf474266c9c68c
chazz6 <chazzyheavy@gmail.com>
2021-03-29 15:37:06 +0700

    Audio Player

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.*;
+
+public class Main {
+
+	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		File file = new File("Numb_life.wav");
+		AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+		Clip clip = AudioSystem.getClip();
+		clip.open(audioStream);
+		clip.start();
+		
+		String response = scanner.next();
+	}
+
+}
